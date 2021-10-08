9e5fac1db63456d34e5645786fb4134743a9d99a
Bruno Cesar Sprotte <bcsprotte.cit@rd.com.br>
2021-10-07 21:12:37 -0300

    teste funcionando

--- tests/E2E/1.test.js
+++ tests/E2E/1.test.js
@@ -8,7 +8,7 @@ describe('Login', () => {
 
   describe('JOIN', () => {
 
-    it.only('shows error message when no room is found', async () => {
+    it('shows error message when no room is found', async () => {
       console.log('preenchendo name');
       await page.type(getDataTestIdSelector('join-name'), 'John Doe')
       console.log('preenchendo room');
@@ -16,25 +16,28 @@ describe('Login', () => {
       console.log('clicando JOIN');
       await page.click(getDataTestIdSelector('join-joinButton'))
       console.log('esperando \'JOIN\'');
-      await page.waitForSelector(getDataTestIdSelector('dialog-not-found-error'))
+      const dialog = await page.waitForSelector(getDataTestIdSelector('dialog-not-found-error-text'));
+      console.log('dialog', dialog)
     })
 
-    it('should redirect to games page when a game is found', async () => {
-      console.log('preenchendo name');
-      await page.type(getDataTestIdSelector('join-name'), 'John Doe')
-      console.log('preenchendo room');
-      await page.type(getDataTestIdSelector('join-roomId'), 'teste')
-      console.log('clicando JOIN');
-      await page.click(getDataTestIdSelector('join-joinButton'))
-      console.log('esperando \'JOIN\'');
-      await expect(page).toHaveURL(/.*teste/);
+    test.only('should redirect to games page when a game is found', async () => {
+      await page.fill('#join-name', 'John Doe')
+      
+      await page.fill('#join-roomId', 'teste')
+      
+      await Promise.all([
+        page.click('#join-joinButton'),
+        page.waitForURL(/.*teste/, { timeout:10000, waitUntil: "domcontentloaded" })  
+      ]);
+      
+      expect(page.url()).toContain('/teste')
     })
 
   })
 
-  describe('JOIN', () => {
+  describe('CREATE', () => {
 
-    it('should redirect to games page when a game is created', async () => {
+    it('should redirect to games page when a game is created', async ({page}) => {
       console.log('preenchendo name');
       await page.type(getDataTestIdSelector('create-name'), 'John Doe')
       console.log('preenchendo room');
